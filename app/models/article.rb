4320497e84dc5a1e8ffab3a820967bf75481863c
Fred Bitenyo <fredbitenyo@gmail.com>
2020-03-27 19:46:24 +0200

    secure password

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: {minimum: 5, maximum: 20}
   validates :description, presence: true, length: {minimum: 10, maximum: 2000}
-  validates :author, presence: true, length: {minimum: 5, maximum: 20}
-  validates :user_id, presence: true 
+  #validates :author, presence: true, length: {minimum: 5, maximum: 20}
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_103525) do
+ActiveRecord::Schema.define(version: 2020_03_27_173320) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_103525) do
     t.string "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "password_digest"
   end
 
 end
