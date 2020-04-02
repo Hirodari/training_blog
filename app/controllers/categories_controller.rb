64ab25ed202622193790e324cee8e54a67a57a3d
Fred Bitenyo <fredbitenyo@gmail.com>
2020-04-02 23:01:11 +0200

    finished category integration

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [:edit, :update, :show]
   before_action :require_admin, except: [:index, :show]
   def index
     @categories = Category.paginate(page: params[:page], per_page: 3)
@@ -19,13 +20,31 @@ class CategoriesController < ApplicationController
       end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "#{@category.name} Updated!"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def show
   end
+
 
   private
     def category_params
       params.require(:category).permit(:name)
     end
+
+    def find_category
+      @category = Category.find(params[:id])
+    end
+
 
     def require_admin
       if !logged_in? || (logged_in? && !current_user.admin?)
